65ecbb6ab593936e88b1faaad34e2f8c79f05c07
valentine.ofili <support@nubeero.com>
2024-08-12 01:36:46 +0100

    added selectionSort class, added new implementation of insertionSort

--- src/main/java/algorithms/InsertionSort.java
+++ src/main/java/algorithms/InsertionSort.java
@@ -1,5 +1,6 @@
 package algorithms;
-
+import java.util.Arrays;
+import java.util.List;
 /**
  * @author valenttine Ofili
  * Implement Insertion Sort algorithm for an array of integer values
@@ -8,11 +9,47 @@ public class InsertionSort {
 
     public static void main(String[] args) {
         int[] testSort={20, 35, -15, 7, 55, 1 , -22,8};
-        for(int x: doSortAscending(testSort)){
-            System.out.println(x);
+//        for(int x: doSortAscending(testSort)){
+//            System.out.println(x);
+//        }
+
+        List<Integer> newList = Arrays.asList(4,2,19,38,9,3);
+        for(Integer i:doIntIns(newList)){
+            System.out.println(i);
         }
     }
 
+
+    public static List<Integer> sortList(List<Integer> unsortedList) {
+        for (int i = 0; i < unsortedList.size(); i++) {
+            int current = i;
+            // gets the smallest element and inserts at current index
+            while (current > 0 && unsortedList.get(current) < unsortedList.get(current - 1)) {
+                int temp = unsortedList.get(current);
+                // swaps current smaller element with the element before it
+                unsortedList.set(current, unsortedList.get(current - 1));
+                unsortedList.set(current - 1, temp);
+                current--;
+            }
+        }
+        return unsortedList;
+    }
+
+    static List<Integer> doIntIns(List<Integer> unsorted){
+        for(int i = 0;i < unsorted.size();i++){
+
+            int currentIndex=i;
+            while (currentIndex  > 0 && unsorted.get(currentIndex-1) > unsorted.get(currentIndex)){
+               Integer temp = unsorted.get(currentIndex);
+
+                unsorted.set(currentIndex,unsorted.get(currentIndex-1));
+                unsorted.set(currentIndex-1,temp);
+                currentIndex--;
+            }
+        }
+        return unsorted;
+
+    }
 
     static int[] doSortAscending(int[] intArray){
         for(int firstUnsortedIndex = 1;firstUnsortedIndex < intArray.length;firstUnsortedIndex++){
